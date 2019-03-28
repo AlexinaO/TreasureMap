cc418bbf56728727dddda46ef7d38c67cd9f6f39
AlexinaO <alexina.orsoni@gmail.com>
2019-03-28 15:25:37 +0100

    IDataMap Interface Creation

---
+++ TreasureMap.DAL/IDataMap.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreasureMap.DAL
+{
+    public interface IDataMap
+    {
+        IEnumerable<Map> GetData();
+
+        void ExitFile(Map map);
+    }
+}
