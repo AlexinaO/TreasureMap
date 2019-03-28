98aa5005c6ae7e57011d86a5741b8ca12f540b69
AlexinaO <alexina.orsoni@gmail.com>
2019-03-28 15:23:37 +0100

    Mountain Class Creation

---
+++ TreasureMap.DAL/Mountain.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreasureMap.DAL
+{
+    public class Mountain
+    {
+        const char MountainCode = 'M';
+        public int MountainVerticalAxis { get; set; }
+        public int MountainHorizontalAxis { get; set; }
+    }
+}
