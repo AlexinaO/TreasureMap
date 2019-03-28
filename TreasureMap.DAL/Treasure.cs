db025b9189e410977fe23513a9df7df318ae1005
AlexinaO <alexina.orsoni@gmail.com>
2019-03-28 15:23:57 +0100

    Treasure Class Creation

---
+++ TreasureMap.DAL/Treasure.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreasureMap.DAL
+{
+    public class Treasure
+    {
+        const char TreasureCode = 'T';
+
+        public int TreasureVerticalAxis { get; set; }
+        public int TreasureHorizontalAxis { get; set; }
+        public int TreasureNumber { get; set; }
+    }
+}
