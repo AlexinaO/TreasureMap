63fe01434c4ad76fbe072211a0f17d214d0771d8
AlexinaO <alexina.orsoni@gmail.com>
2019-03-28 15:23:22 +0100

    Map Class Creation

---
+++ TreasureMap.DAL/Map.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace TreasureMap.DAL
+{
+    public class Map
+    {
+        const char MapCode = 'C';
+
+        public int WidthBoxesNumber { get; set; }
+        public int HeightBoxesNumber { get; set; }
+
+        public List<Adventurer> Adventurers { get; set; }
+        public List<Mountain> Moutains { get; set; }
+        public List<Treasure> Treasures { get; set; }
+    }
+}
