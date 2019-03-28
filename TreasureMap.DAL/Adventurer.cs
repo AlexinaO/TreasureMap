f47d0eb72f62e9df137a7c2258036fcca12dad9c
AlexinaO <alexina.orsoni@gmail.com>
2019-03-28 15:23:03 +0100

    Adventurer Class Creation

---
+++ TreasureMap.DAL/Adventurer.cs
@@ -0,0 +1,12 @@
+﻿namespace TreasureMap.DAL
+{
+    public class Adventurer
+    {
+        const char AdventurerCode = 'A';
+        public string Name { get; set; }
+        public int AdventurerHorizontalAxis { get; set; }
+        public int AdventurerVerticalAxis { get; set; }
+        public string Orientation { get; set; }
+        public string Movement { get; set; }
+    }
+}
