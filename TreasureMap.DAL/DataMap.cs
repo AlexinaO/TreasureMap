86c2fdc952d716345ae5d4c2745cdfd544fbe6b6
Alexina <alexina.orsoni@gmail.com>
2019-03-29 16:46:53 +0100

    DataMap class : minous modifications

--- TreasureMap.DAL/DataMap.cs
+++ TreasureMap.DAL/DataMap.cs
@@ -6,20 +6,20 @@ namespace TreasureMap.DAL
     public class DataMap : IDataMap
     {
         const string FilePath = "TreasureMap.txt";
-        const string FieldSeparator = " - ";
+        const char FieldSeparator = '-';
 
-        private Map Map;
+        private Map map;
         private List<Adventurer> adventurers;
         private List<Treasure> treasures;
         private List<Mountain> mountains;
 
         public Map GetDataMap()
         {
-            if (this.Map == null)
+            if (this.map == null)
             {
                 ReadFile();
             }
-            return Map;
+            return this.map;
         }
 
         public IEnumerable<Adventurer> GetAdventurersList()
@@ -31,7 +31,7 @@ namespace TreasureMap.DAL
             return adventurers;
         }
 
-        public IEnumerable<Treasure>GetTreasuresList()
+        public IEnumerable<Treasure> GetTreasuresList()
         {
             if (this.treasures == null)
             {
@@ -42,7 +42,7 @@ namespace TreasureMap.DAL
 
         public IEnumerable<Mountain> GetMountainsList()
         {
-            if(this.mountains == null)
+            if (this.mountains == null)
             {
                 ReadFile();
             }
@@ -57,75 +57,83 @@ namespace TreasureMap.DAL
 
         private void ReadFile()
         {
-            this.Map = new Map();
+            this.map = new Map();
             this.adventurers = new List<Adventurer>();
             this.mountains = new List<Mountain>();
             this.treasures = new List<Treasure>();
             if (File.Exists(FilePath))
             {
-                using (var reader = new StreamReader(FilePath))
+                var lines = File.ReadAllLines(FilePath);
                 {
-                    while (!reader.EndOfStream)
+                    foreach (var textLine in lines)
                     {
-                        string[] readText = File.ReadAllLines(FilePath);
-                        foreach (var textLine in readText)
-                            if (textLine.StartsWith("#"))
+                        if (textLine.StartsWith("#"))
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            var fields = textLine.Split(FieldSeparator);
+
+                            if (textLine.StartsWith("C"))
                             {
+                                if (this.map.Code == null)
+                                {
+                                    //var map = new Map();
+                                    map.Code = fields[0];
+                                    map.WidthBoxesNumber = int.Parse(fields[1]);
+                                    map.HeightBoxesNumber = int.Parse(fields[2]);
+                                    continue;
+                                }
+                                else
+                                {
+                                    throw new MessageException("A Map already exists in the file. Please correct the file to have only one map.");
+                                }
+                            }
+                            if (textLine.StartsWith("M"))
+                            {
+                                var mountain = new Mountain();
+                                mountain.Code = fields[0];
+                                mountain.MountainHorizontalAxis = int.Parse(fields[1]);
+                                mountain.MountainVerticalAxis = int.Parse(fields[2]);
+
+                                mountains.Add(mountain);
                                 continue;
                             }
-                            else
+                            if (textLine.StartsWith("T"))
                             {
-                                var fields = textLine.Split(FieldSeparator);
+                                var treasure = new Treasure();
+                                treasure.Code = fields[0];
+                                treasure.TreasureHorizontalAxis = int.Parse(fields[1]);
+                                treasure.TreasureVerticalAxis = int.Parse(fields[2]);
+                                treasure.TreasureNumber = int.Parse(fields[3]);
 
-                                if (textLine.StartsWith("C"))
-                                {
-                                    if (this.Map == null)
-                                    {
-                                        var map = new Map();
-                                        map.Code = fields[0];
-                                        map.WidthBoxesNumber = int.Parse(fields[1]);
-                                        map.HeightBoxesNumber = int.Parse(fields[2]);
-                                    }
-                                    else
-                                    {
-                                        throw new MessageException("A Map already exists in the file. Please correct the file to have only one map.");
-                                    }
-                                }
-                                if (textLine.StartsWith("M"))
-                                {
-                                    var mountain = new Mountain();
-                                    mountain.Code = fields[0];
-                                    mountain.MountainHorizontalAxis = int.Parse(fields[1]);
-                                    mountain.MountainVerticalAxis = int.Parse(fields[2]);
-
-                                    mountains.Add(mountain);
-                                }
-                                if (textLine.StartsWith("T"))
-                                {
-                                    var treasure = new Treasure();
-                                    treasure.Code = fields[0];
-                                    treasure.TreasureHorizontalAxis = int.Parse(fields[1]);
-                                    treasure.TreasureVerticalAxis = int.Parse(fields[2]);
-                                    treasure.TreasureNumber = int.Parse(fields[3]);
-
-                                    treasures.Add(treasure);
-                                }
-                                if (textLine.StartsWith("A"))
-                                {
-                                    var adventurer = new Adventurer();
-                                    adventurer.Code = fields[0];
-                                    adventurer.Name = fields[1];
-                                    adventurer.AdventurerHorizontalAxis = int.Parse(fields[2]);
-                                    adventurer.AdventurerVerticalAxis = int.Parse(fields[3]);
-                                    adventurer.Orientation = fields[4];
-                                    adventurer.Movement = fields[5];
-
-                                    adventurers.Add(adventurer);
-                                }
+                                treasures.Add(treasure);
+                                continue;
                             }
+                            if (textLine.StartsWith("A"))
+                            {
+                                var adventurer = new Adventurer();
+                                adventurer.Code = fields[0];
+                                adventurer.Name = fields[1];
+                                adventurer.AdventurerHorizontalAxis = int.Parse(fields[2]);
+                                adventurer.AdventurerVerticalAxis = int.Parse(fields[3]);
+                                adventurer.Orientation = fields[4];
+                                adventurer.Movement = fields[5];
+
+                                adventurers.Add(adventurer);
+                                continue;
+                            }
+                        }
                     }
                 }
             }
+            else
+            {
+                throw new MessageException("Le fichier TreasureMap.txt n'existe pas dans le dossier TreasureMap\\bin\\Debug");
+            }
         }
+
     }
+
 }
