5b1c33b917a34d549649703ed1e836ebdae4c95b
Alexina <alexina.orsoni@gmail.com>
2019-03-29 11:56:16 +0100

    ServiceData : Implemantion of IServiceDataMembers

--- TreasureMap.Business/ServiceData.cs
+++ TreasureMap.Business/ServiceData.cs
@@ -15,5 +15,25 @@ namespace TreasureMap.Business
         {
             this.data = new DataMap();
         }
+
+        public Map GetMap()
+        {
+            return this.data.GetDataMap();
+        }
+
+        public IEnumerable<Adventurer> GetAdventurers()
+        {
+            return this.data.GetAdventurersList();
+        }
+
+        public IEnumerable<Treasure> GetTreasures()
+        {
+            return this.data.GetTreasuresList();
+        }
+
+        public IEnumerable<Mountain> GetMountains()
+        {
+            return this.data.GetMountainsList();
+        }
     }
 }
