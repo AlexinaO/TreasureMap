8d21842424f6409fd1a8c2a273512ed8a3a5258c
Alexina <alexina.orsoni@gmail.com>
2019-03-29 09:49:35 +0100

    Map Class - Replacement of const char by Code - Lists in commentary (before deleting ?)

--- TreasureMap.DAL/Map.cs
+++ TreasureMap.DAL/Map.cs
@@ -1,16 +1,12 @@
-﻿using System.Collections.Generic;
-
-namespace TreasureMap.DAL
+﻿namespace TreasureMap.DAL
 {
     public class Map
     {
-        const char MapCode = 'C';
-
+        public string Code { get; set; }
         public int WidthBoxesNumber { get; set; }
         public int HeightBoxesNumber { get; set; }
-
-        public List<Adventurer> Adventurers { get; set; }
-        public List<Mountain> Moutains { get; set; }
-        public List<Treasure> Treasures { get; set; }
+        //public List<Adventurer> Adventurers { get; set; }
+        //public List<Mountain> Moutains { get; set; }
+        //public List<Treasure> Treasures { get; set; }
     }
 }
