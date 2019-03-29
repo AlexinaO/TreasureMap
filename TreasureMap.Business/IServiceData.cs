b02681eaf8923c302a2a12459265fe8a4e48bfbe
Alexina <alexina.orsoni@gmail.com>
2019-03-29 11:44:50 +0100

    Add of a Business Layer with services

---
+++ TreasureMap.Business/IServiceData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TreasureMap.DAL;
+
+namespace TreasureMap.Business
+{
+    public interface IServiceData
+    {
+        Map GetMap();
+        IEnumerable<Treasure> GetTreasures();
+        IEnumerable<Adventurer> GetAdventurers();
+        IEnumerable<Mountain> GetMountains();
+    }
+}
---
+++ TreasureMap.Business/ServiceData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TreasureMap.DAL;
+
+namespace TreasureMap.Business
+{
+    public class ServiceData : IServiceData
+    {
+        private readonly IDataMap data;
+
+        public ServiceData()
+        {
+            this.data = new DataMap();
+        }
+    }
+}
