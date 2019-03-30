b5a3be1265362e48ca659b3e5c107e728f64a432
Alexina <alexina.orsoni@gmail.com>
2019-03-30 22:04:18 +0100

    Add of 2 parameters in Adventurer Class: - TreasureFound = number of treasures found by the adventurer - MovementNumber = number of movement for the adventurer

--- TreasureMap.DAL/Adventurer.cs
+++ TreasureMap.DAL/Adventurer.cs
@@ -2,11 +2,16 @@
 {
     public class Adventurer
     {
-		public string Code { get; set; }
+        public string Code { get; set; }
         public string Name { get; set; }
         public int AdventurerHorizontalAxis { get; set; }
         public int AdventurerVerticalAxis { get; set; }
         public string Orientation { get; set; }
         public string Movement { get; set; }
+        public int TreasureFound { get; set; }
+
+        public int MovementNumber { get { return Movement.Length; } }
+
+        
     }
 }
