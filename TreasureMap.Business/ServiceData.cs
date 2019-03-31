8eac4b5c1a0f802ed76a864301bf0cdbc7ab6adb
Alexina <alexina.orsoni@gmail.com>
2019-04-01 00:00:54 +0200

    [ServiceData] - Methods to write a file
    - CreateExitData()

--- TreasureMap.Business/ServiceData.cs
+++ TreasureMap.Business/ServiceData.cs
@@ -35,5 +35,27 @@ namespace TreasureMap.Business
         {
             return this.data.GetMountainsList();
         }
+
+        /// <summary>
+        /// Method to create the data which will be write in the exit txt file
+        /// </summary>
+        /// <param name="exitDataMap"></param>
+        public void CreateExitData(DataMap exitDataMap)
+        {
+            if(exitDataMap.currentMap == null)
+            {
+                throw new MessageException("Il n'y a pas de carte aux trésors valide.");
+            }
+            if (exitDataMap.adventurers == null)
+            {
+                throw new MessageException("Il n'y a pas de liste d'aventuriers valide.");
+            }
+            if(exitDataMap.treasures == null)
+            {
+                throw new MessageException("Il n'y a pas de liste de trésors valide " +
+                    "donc les aventuriers ne pourront pas récolter de trésors.");
+            }
+            this.data.SaveExitData(exitDataMap);
+        }
     }
 }
