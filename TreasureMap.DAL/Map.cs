e3c67fe487c091fdc3e693209d37cde866eec4a3
Alexina <alexina.orsoni@gmail.com>
2019-04-01 00:01:38 +0200

    [Map.cs] - Deletion of the unuseful commentaries

--- TreasureMap.DAL/Map.cs
+++ TreasureMap.DAL/Map.cs
@@ -5,8 +5,5 @@
         public string Code { get; set; }
         public int WidthBoxesNumber { get; set; }
         public int HeightBoxesNumber { get; set; }
-        //public List<Adventurer> Adventurers { get; set; }
-        //public List<Mountain> Moutains { get; set; }
-        //public List<Treasure> Treasures { get; set; }
     }
 }
